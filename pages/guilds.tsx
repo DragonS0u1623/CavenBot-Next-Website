b1ead6336648796919c689bddd11e1afaa6d3114
DragonS0u1623 <DragonS0u1623@gmail.com>
2023-03-05 19:55:43 -0500

    Added placeholder guilds page

---
+++ pages/guilds.tsx
@@ -0,0 +1,8 @@
+export default function Guilds() {
+    return (
+        <>
+            <h1>This feature is under construction</h1>
+            <p>Sorry for the inconvenience but this feature is currently under construction and won&apos;t be released for a little while.</p>
+        </>
+    )
+}
